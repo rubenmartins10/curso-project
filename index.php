da20e727639e55e3d5964696251936803b8c96e8
Rúben Martins <rubendavidsilvamartins@gmail.com>
2026-02-20 00:31:49 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+$nome = 'David';
+$idade = 'Desconhecida';
+
+?><h1><?php echo $nome;?></h1><?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device=width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title> Micro CMS </title>
+  <link rel="stylesheet" href="assets/style.css"> </head>
+
+<body>
+<nav id="site-navigation" role="navigation" class="row row-center">
+  <div class="column">
+    <h1>
+      <a href="index.php">Micro CMS</a>
+    </h1>
+    <ul class="main-menu column clearfix">
+    </ul>
+  </div>
+
+  <div id="content">
+
+  </div>
+
+  <footer id="footer">
+    <div id="inner-footer">
+      Curso de Introdução a PHP
+    </div>
+  </footer>
+</body>
